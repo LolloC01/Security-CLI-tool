7b96eac989af1218b72362dbb5014300328be99b
LolloC01 <colittolorenzo@gmail.com>
2026-08-25 21:09:35 +0200

    app Typer

---
+++ src/security_cli/cli/main.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2026 Lorenzo Colitto
+# Questo codice è distribuito sotto i termini della licenza MIT.
+# Vedi il file LICENSE nella radice del progetto per il testo completo.
+
+import typer
+#from commands.hash import app as hash_app
+
+app = typer.Typer(
+    name="appsec",
+    help="Application Security Toolkit",
+    no_args_is_help=True,
+)
+
+app.add_typer(
+    hash_app,
+    name="hash",
+)
+
+if __name__ == "__main__":
+    app()
